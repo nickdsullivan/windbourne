cffa6545eda6ffe2201d218991991cc019b85303
nickdsullivan <nickdsullivan@gmail.com>
2025-03-04 11:46:25 -0500

    Refactoring

---
+++ weather-balloon-tracker/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, World! 👋</h1>
+      <p>Welcome to my Next.js app!</p>
+    </main>
+  );
+}
