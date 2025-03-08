f49f29740c0f9e32b91ea32d1622a5504018241d
nickdsullivan <nickdsullivan@gmail.com>
2025-03-08 15:41:31 -0500

    some logging

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
 
 @app.get("/refresh-data")
 def refresh_data():
+    print("Refreshing Data")
     clear_folder("./images")
     clear_folder("./wind_column")
     dc.clear()
@@ -34,6 +35,7 @@ def refresh_data():
 
 @app.get("/balloon-map")
 def get_balloon_map(hour=0):
+    print("balloon-map")
     hour = int(hour)
     filename = f"images/current_positions_{hour}.png" 
     if not was_file_created_last_hour(filename):
@@ -44,21 +46,11 @@ def get_balloon_map(hour=0):
         return {"error": "Image not found"}
     
 
-@app.get("/balloon-map")
-def get_single_balloon_map(hour=0):
-    hour = int(hour)
-    filename = f"images/current_positions_{hour}.png" 
-    if not was_file_created_last_hour(filename):
-        visualizer.create_current_map(df,hour)
-    if os.path.exists(filename):
-        return FileResponse(filename, media_type="image/png")
-    else:
-        return {"error": "Image not found"}
-
 
 
 @app.get("/wind-column")
 def get_balloon_map(balloon_id, hour=0):
+    print(f"wind-column balloon_id:{balloon_id}, hour:{hour}")
     hour = int(hour)
     balloon_id = int(balloon_id)
     lat, long, elevation = dc.get_balloon_location(balloon_id, hour)
@@ -80,6 +72,7 @@ def get_balloon_map(balloon_id, hour=0):
 
 @app.get("/get-positions")
 def get_positions(hour=0):
+    print(f"get-positions hour:{hour}")
     hour = int(hour)
     return JSONResponse(content= visualizer.get_positions(df,hour))
 
@@ -87,11 +80,13 @@ def get_positions(hour=0):
 
 @app.get("/get-refresh-time")
 def get_refresh_time():
+    print(f"get-refresh-time")
     return {"time_utc" : dc.latest_collection_time}
     
 
 @app.get("/balloon-details")
 def get_balloon_details(balloon_id, hour = 0):
+    print(f"balloon-details")
     balloon_id = int(balloon_id)
     hour = int(hour)
     return JSONResponse(content= dc.get_balloon_details_as_json(balloon_id,hour))
@@ -99,6 +94,7 @@ def get_balloon_details(balloon_id, hour = 0):
 
 @app.get("/single-balloon-map-navigator")
 def single_balloon_map_navigator(balloon_id, hour = 0, x = -1, y = -1):
+    print(f"single-balloon-map-navigator")
     balloon_id = int(balloon_id)
     hour = int(hour)
     filename = f"images/single_{balloon_id}_{hour}.png"
@@ -122,6 +118,7 @@ last_node = None
 
 @app.get("/start-navigation")
 def start_navigation(lat, long, alt, t_lat, t_long, t_alt, max_iters = 20):
+    print(f"start-navigation")
     max_iters = max(min(int(max_iters),50),1)
     lat = float(lat)
     long = float(long)
@@ -156,6 +153,7 @@ def create_path_map(lat, long, alt, t_lat, t_long, t_alt, last_node):
 
 @app.get("/get-directions-map")
 def get_directions_map(balloon_id, hour = 0, x = -1, y = -1):
+    print(f"directions map")
     balloon_id = int(balloon_id)
     hour = int(hour)
     filename = f"images/current_path.png"
