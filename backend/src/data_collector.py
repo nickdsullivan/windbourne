a2ad6dbbda02b208bd6825f227ac297c255ce5ac
nickdsullivan <nickdsullivan@gmail.com>
2025-03-09 19:43:14 -0400

    More debugging

--- backend/src/data_collector.py
+++ backend/src/data_collector.py
@@ -368,8 +368,8 @@ class DataCollector:
         return {"lat" : df["Latitude"].iloc[0], "long" : df["Longitude"].iloc[0], "alt":  df["Elevation"].iloc[0], "speed": df["Speed"].iloc[0], "bearing": df["Bearing"].iloc[0]}
 
     def hour_unavailable(self,hour):
-        print(self.balloon_data[self.balloon_data["Hour"] == hour].isna().all())
-        return self.balloon_data[self.balloon_data["Hour"] == hour].isna().all().all()
+        print(self.balloon_data[self.balloon_data["Hour"] == hour]["Latitude"].isna().all())
+        return self.balloon_data[self.balloon_data["Hour"] == hour]["Latitude"].isna().all()
 
     def exterpolate_left(self, hour):
         print("Left")
